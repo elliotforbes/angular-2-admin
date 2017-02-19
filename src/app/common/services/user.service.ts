2ef4e6b20f0ae521478462987b66049c1954e162
= <emforce@live.com>
2017-02-19 21:06:54 +0000

    removed merge issues

--- src/app/common/services/user.service.ts
+++ src/app/common/services/user.service.ts
@@ -9,16 +9,7 @@ export class UserService {
   constructor(private http: Http) { }
 
   getUsers () {
-<<<<<<< HEAD
-    // let headers = new Headers();
-    // var authToken: String;
-    // authToken = 'NLqjPLg94MlBoTCekz4HOHTIh2by3LQRTPo5kgce';
-    // headers.append('Authorization', `Bearer ${authToken}`);
-
     return this.http.get(environment.api_url + "/api/users")
-=======
-    return this.http.get("http://localhost:8000/api/users")
->>>>>>> 41958190f2297586cf711b06b4fc57dd25fe1e17
       .map((res:Response) => res.json())
       .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
   }
